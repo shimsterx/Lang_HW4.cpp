4af3d3a8306c980af1fbbc8855af5a2243fdc33b
shimsterx <157174526+shimsterx@users.noreply.github.com>
2024-03-18 17:40:18 -0500

    Add files via upload

---
+++ Lang_HW4 (1).cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <iomanip> 
+
+using namespace std;
+
+int main() {
+    cout << "Farmer Boy" << endl << endl;
+    
+    cout << "Table of Contents" << endl << endl;
+    
+    cout << right << setw(3) << "1" << "  " << left << setw(30) << "Chapter 1: Farmer Boy" << right << setw(5) << "9" << endl;
+    cout << right << setw(3) << "2" << "  " << left << setw(30) << "Chapter 2: Winter Days" << right << setw(5) << "21" << endl;
+    cout << right << setw(3) << "3" << "  " << left << setw(30) << "Chapter 3: Christmas" << right << setw(5) << "35" << endl;
+    cout << right << setw(3) << "4" << "  " << left << setw(30) << "Chapter 4: School Days" << right << setw(5) << "49" << endl;
+    cout << right << setw(3) << "5" << "  " << left << setw(30) << "Chapter 5: Springtime" << right << setw(5) << "63" << endl;
+   
+    cout << endl;
+
+    return 0;
+}
+
+
